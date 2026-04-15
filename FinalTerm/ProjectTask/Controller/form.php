2177cd3e0b80609a1c3275f33e48316e9a028d0d
marup00800 <marup.hossain00900@gmail.com>
2026-04-15 09:30:10 +0600

    Added FinalTerm folder

---
+++ FinalTerm/ProjectTask/Controller/form.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+$username = $_SESSION["username"] ?? "";
+$email = $_SESSION["email"] ?? "";
+
+$usernameErr = $_SESSION["usernameErr"] ?? "";
+$nameErr = $_SESSION["nameErr"] ?? "";
+$emailErr = $_SESSION["emailErr"] ?? "";
+
+unset($_SESSION["usernameErr"], $_SESSION["nameErr"], $_SESSION["emailErr"], $_SESSION["phoneErr"]);
+?>
+
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+
+<h2>User Form</h2>
+
+<form method="post" action="../Controller/formValidation.php">
+
+<table>
+<tr>
+    <td>Username</td>
+    <td><input type="text" name="username" value="<?php echo $username; ?>"></td>
+</tr>
+
+<tr>
+    <td>Email</td>
+    <td><input type="text" name="email" value="<?php echo $email; ?>"></td>
+</tr>
+
+<tr>
+    <td></td>
+    <td><input type="submit" value="Submit"></td>
+</tr>
+
+</table>
+
+</form>
+
+</body>
+</html>
---
+++ FinalTerm/ProjectTask/view/formvalodation.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$username = $_POST["username"] ?? "";
+$email = $_POST["email"] ?? "";
+
+$hasError = false;
+
+if(empty($username)){
+    $_SESSION["usernameErr"] = "Username required";
+    $hasError = true;
+}else{
+    $_SESSION["username"] = $username;
+}
+
+if(empty($email)){
+    $_SESSION["emailErr"] = "Email required";
+    $hasError = true;
+}else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $_SESSION["emailErr"] = "Invalid email format";
+    $hasError = true;
+}else{
+    $_SESSION["email"] = $email;
+}
+
+
+if($hasError){
+    header("Location: ../View/form.php");
+    exit();
+}
+
+echo "<h2>Form Submitted Successfully</h2>";
+echo "Username: " . $username . "<br>";
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Phone: " . $phone . "<br>";
+?>
